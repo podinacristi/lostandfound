dfe3fc7b3fa724a655493798be4e270b56fd8afe
Ovidiu Apostol <ovidiu.apostol@bgasoft.net>
2016-02-10 14:24:41 -0500

    atk

---
+++ config-auto.php
@@ -0,0 +1,6 @@
+<?php
+$config['seed'] = '56bb866f74ebb'; 
+$config['title'] = 'LostAndFound'; 
+$config['name'] = 'LostAndFound'; 
+$config['certificate'] = 'KdopmtZATBoaHThtk3UeKkkz73vdH8nreIQxWyo5Q4NRP/LGtVQOm53O0Y0bE8OYSGyiZJnwXdOWGiMGMskgUQzWDP5RN0iT/o3m70zfl2uD9oihPuI7MSXHW7/01ypd6aqoRTqUpG7dJvpgijxwSXEJmLiL+Zuk2u3NRxP7jwu0naXbPHcRC5rzkaIqfZq6znfvo1lcGL3ZSk5RfU2ru0aVSnlvtxZ1OW3DRXy2ghq71BAM31pafEd7Ss/z5Ge7E+UfeU2XuFD/Ih86Y+XC6r6N3ok1e31lJfXNnaCWPnHeZyc9eABdfzVma5EsjfTviBLPVCYm7yssJq7DiSLEeg==:eyJjZXJ0X2V4cGlyZXNfZHRzIjoxNDU1MTQ0Njg3LCJjZXJ0X2lzc3VlZF9kdHMiOjE0NTUxMzAyODcsInBsYW4iOm51bGwsInVpaWQiOiIzYjgyNmViNzYxNWMwMGU4MjQyNWM2MzFkMjQwNDFlMmJjMDhhMzg3IiwidmVyc2lvbiI6IjIifQ=='; 
+$config['installation_hash'] = '3b826eb7615c00e82425c631d24041e2bc08a387'; 
